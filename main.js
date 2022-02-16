5777034abe0269cfecbd0a6b3f2c24f459ed43bf
mdsumonali372 <mdsumonali026@gmail.com>
2022-02-17 02:05:38 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+
+$(document) .ready(function(){
+
+//Portfolio Filter		
+$('.card-list').imagesLoaded(function () {
+    // init Isotope
+    var $grid = $('.card-list').isotope({
+      itemSelector: '.single-card-item',
+      percentPosition: true,
+      masonry: {
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: '.grid-sizer'
+      }
+    });    
+    // filter items on button click
+    $('.filter-menu').on('click', 'li', function () {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({
+        filter: filterValue
+      });
+    });
+  });
+  //for menu active class
+  $('.filter-menu li').on('click', function (event) {
+    $(this).siblings('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+    event.preventDefault();
+  });
+
+
+
+  $(".count").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            now = Number(Math.ceil(now)).toLocaleString('en');
+                                  $(this).text(now);
+          },
+        }
+      );
+  });
+
+
+})
